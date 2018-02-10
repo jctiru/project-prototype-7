ba58c35a9fd35526bf9b0131e9753a6ce11ebfd4
jctiru <jonathanchristiantiru@gmail.com>
2018-02-10 20:23:55 +0800

    Added edit/delete on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,32 @@
                     @endif
                     <a href="/posts/create" class="btn btn-primary">Create Post</a>
                     <h3>Your Blog Posts</h3>
+                    @if (count($posts)>0)
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach ($posts as $post)
+                                <tr>
+                                    <td>{{$post->title}}</td>
+                                    <td><a class="btn btn-default" href="/posts/{{$post->id}}/edit">Edit</a></td>
+                                    <td>
+                                        <form action="/posts/{{$post->id}}" method="POST" class="pull-right">
+                                            {{ csrf_field() }}
+                                            {{-- For DELETE requests --}}
+                                            {{-- <input name="_method" type="hidden" value="DELETE"> --}}
+                                            {{ method_field('DELETE') }}
+                                            <input type="submit" class="btn btn-danger" value="Delete">
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    @else
+                        <p>You have no posts</p>
+                    @endif
                 </div>
             </div>
         </div>
