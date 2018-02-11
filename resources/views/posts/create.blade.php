e1565510c3a2f9a2f2db7e42a730a8e9a87ad220
jctiru <jonathanchristiantiru@gmail.com>
2018-02-11 22:50:18 +0800

    Added image upload

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts/app')
 @section('content')
 	<h1>Create Post</h1>
-	<form action="/posts" method="POST">
+	<form action="/posts" method="POST" enctype="multipart/form-data">
 		{{ csrf_field() }}
 		<div class="form-group">
 			<label for="title">Title</label>
@@ -10,6 +10,9 @@
 		<div class="form-group">
 			<label for="body">Body</label>
 			<textarea id="article-ckeditor" placeholder="Body Text" class="form-control" name="body">{{ old('body') }}</textarea>
+		</div>
+		<div class="form-group">
+			<input type="file" name="cover_image" accept="image/*"  class="form-control-file form-control-sm">
 		</div>
 		<input type="submit" class="btn btn-primary" value="Submit">
 	</form>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts/app')
 @section('content')
 	<h1>Edit Post</h1>
-	<form action="/posts/{{$post->id}}" method="POST">
+	<form action="/posts/{{$post->id}}" method="POST" enctype="multipart/form-data">
 		{{ csrf_field() }}
 		{{-- For PUT requests --}}
 		{{-- <input name="_method" type="hidden" value="PUT"> --}}
@@ -13,6 +13,9 @@
 		<div class="form-group">
 			<label for="body">Body</label>
 			<textarea id="article-ckeditor" placeholder="Body Text" class="form-control" name="body">{{$post->body}}</textarea>
+		</div>
+		<div class="form-group">
+			<input type="file" name="cover_image" accept="image/*"  class="form-control-file form-control-sm">
 		</div>
 		<input type="submit" class="btn btn-primary" value="Submit">
 	</form>
